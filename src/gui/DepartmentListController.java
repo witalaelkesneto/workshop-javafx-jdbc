d94f829819191cde2f5570cb605ef4cacf2c2cb7
Witalaelkes Neto <witalaelkes.neto@icloud.com>
2022-09-07 15:00:12 -0300

    Department Combo Box

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -111,6 +111,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 			dialogstage.initModality(Modality.WINDOW_MODAL);
 			dialogstage.showAndWait();
 		} catch (IOException e) {
+			e.printStackTrace();
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
